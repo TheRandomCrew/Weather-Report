b27533ee79f2915e856b8393d26be21cf8b38185
Emmanuel Azocar <azocarmel@gmail.com>
2022-10-08 20:40:58 -0400

    fix: solved comments

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,51 +1,68 @@
-import Turnstone from 'turnstone'
-import cityList from '../data/cityList.json'
+import { useState } from "react";
+import Turnstone from "turnstone";
+import cityList from "../data/cityList.json";
 
-const SearchInput = ({ onSelect }) => {
-	const listbox = {
-		data: cityList.map((city) => ({
-			...city,
-			displayName: `${city.name}, ${city.country}`,
-		})),
-		searchType: 'startswith',
-		displayField: 'displayName',
-	}
+const styles = {
+  input: "searchInput",
+  query: "searchInputQuery",
+  listbox: "searchResults",
+  item: "searchResultsItem",
+  highlightedItem: "searchResultsItemHover",
+};
 
-	return (
-		<div className="lg:px-36 md:px-16 px-5">
-			<Turnstone
-				debounceWait={250}
-				id="search"
-				listbox={listbox}
-				listboxIsImmutable={true}
-				matchText={true}
-				maxItems={10}
-				name="search"
-				noItemsMessage="We found no places that match your search"
-				placeholder="Enter city or region"
-				typeahead={true}
-				styles={{
-					input:
-						'w-full h-12 border-b-2 border-b-white py-2 px-5 text-xl outline-none bg-transparent text-white',
-					inputFocus:
-						'w-full h-12 border-b-2 border-b-white py-2 px-5 text-xl outline-none bg-transparent text-white',
-					query: 'text-slate-800 placeholder-slate-400 text-xl font-medium',
+const SearchInput = ({ onSearch }) => {
+  const [queryCityId, setQueryCityId] = useState(null);
 
-					cancelButton: `absolute w-10 h-12 inset-y-0 left-0 items-center justify-center z-10 text-blue-400 inline-flex sm:hidden`,
-					clearButton:
-						'absolute inset-y-0 right-0 w-8 inline-flex items-center justify-center text-slate-400 hover:text-rose-400',
-					listbox:
-						'w-full bg-white sm:border sm:border-blue-300 text-left sm:mt-2 p-2 sm:drop-shadow-xl',
-					groupHeading:
-						'cursor-default mt-2 mb-0.5 px-1.5 uppercase text-sm text-rose-300',
-					item: 'cursor-pointer p-1.5 text-lg overflow-ellipsis overflow-hidden text-slate-700',
-					highlightedItem:
-						'cursor-pointer p-1.5 text-lg overflow-ellipsis overflow-hidden text-slate-700 rounded bg-blue-50',
-				}}
-				onSelect={(e) => e?.id && onSelect(e.id)}
-			/>
-		</div>
-	)
-}
+  const listbox = {
+    data: cityList.map((city) => ({
+      ...city,
+      displayName: `${city.name}, ${city.country}`,
+    })),
+    searchType: "startswith",
+    displayField: "displayName",
+  };
 
-export default SearchInput
+  return (
+    <div className="lg:px-36 md:px-16 px-5">
+      <div className="relative">
+        <Turnstone
+          debounceWait={250}
+          id="search"
+          listbox={listbox}
+          listboxIsImmutable={true}
+          matchText={true}
+          maxItems={10}
+          noItemsMessage="We found no places that match your search"
+          placeholder="Enter city or region"
+          typeahead={false}
+          styles={styles}
+          onSelect={(event) => event?.id && setQueryCityId(event.id)}
+          onEnter={() => onSearch(queryCityId)}
+        />
+        <button
+          type="button"
+          className="searchButton"
+          onClick={() => onSearch(queryCityId)}
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import SearchInput from '../components/SearchInput'
-import weatherTypes from '../data/weatherTypes'
-import { getWeatherByCityId, getWeatherByCoords } from '../services/api'
-import style from '../styles/Home.module.css'
+import SearchInput from "../components/SearchInput";
+import weatherTypes from "../data/weatherTypes";
+import { getWeatherByCityId, getWeatherByCoords } from "../services/api";
+import style from "../styles/Home.module.css";
 
 const Home = () => {
-	const [weatherData, setWeatherData] = useState({})
-	const [background, setBackground] = useState('')
+  const [weatherData, setWeatherData] = useState({});
+  const [background, setBackground] = useState("");
 
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(async (position) => {
-			const { data } = await getWeatherByCoords(
-				position.coords.latitude,
-				position.coords.longitude
-			)
-			setWeatherData(data)
-			setBackground(weatherTypes[data.weather[0].icon])
-		})
-	}, [])
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { data } = await getWeatherByCoords(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setWeatherData(data);
+      setBackground(weatherTypes[data.weather[0].icon]);
+    }, console.error);
+  }, []);
 
-	const onSelect = async (cityId) => {
-		const { data } = await getWeatherByCityId(cityId)
-		setWeatherData(data)
-		setBackground(weatherTypes[data.weather[0].icon])
-	}
+  const onSearch = async (cityId) => {
+    const { data } = await getWeatherByCityId(cityId);
+    setWeatherData(data);
+    setBackground(weatherTypes[data?.weather[0].icon]);
+  };
 
-	return (
-		<div className={style.app}>
-			<div className={`${style.card} ${style[background]}`}>
-				<div className={style.info}>
-					<SearchInput onSelect={onSelect} />
-					<h1 className="text-3xl">{weatherData.name}</h1>
-				</div>
-				<div className={style.sidebar}>
-					<h1 className="text-3xl">Hello world!</h1>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={style.app}>
+      <div
+        className={`${style.card} ${style[background]}`}
+      >
+        <div className={style.info}>
+          <SearchInput onSearch={onSearch} />
+          <h1 className="text-3xl">{weatherData.name}</h1>
+        </div>
+        <div className={style.sidebar}>
+          <h1 className="text-3xl">More Info</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
