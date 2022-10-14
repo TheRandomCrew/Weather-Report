21376f50fbd1a7fe4e9c9ca293ce0d5b218cd0c2
Jose Yepez <josyepe97@gmail.com>
2022-10-14 15:18:44 -0500

    Revert "Update index.js"
    This reverts commit da12660954a5e29f56311816d6eccef9a27c421d.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import { Container } from 'postcss';
 import { useContext, useEffect, useState } from 'react';
 import Loading from '../components/Loading';
+
 import SearchInput from '../components/SearchInput';
 import Sidebar from '../components/Sidebar';
-import Principal from '../components/Principal';
 import { DEFAULT_CITY_ID } from '../constants';
 import TemperatureUnitContext from '../context/TemperatureUnit';
 import weatherTypes from '../data/weatherTypes';
@@ -11,9 +10,8 @@ import { kelvinToOthers } from '../lib/converter';
 import { getWeatherByCityId, getWeatherByCoords } from '../services/api';
 import style from '../styles/Home.module.css';
 
-
 const Home = () => {
-  const { temperatureUnit } = useContext(TemperatureUnitContext);
+  const { isCelsius } = useContext(TemperatureUnitContext);
   const [weatherData, setWeatherData] = useState({});
   const [background, setBackground] = useState('');
   const [loading, setLoading] = useState(true);
@@ -53,14 +51,12 @@ const Home = () => {
           <div className={style.info}>
             <SearchInput onSearch={onSearch} />
             <h1 className="text-3xl">{weatherData.name}</h1>
+            {isCelsius ? (
+              <h1>{kelvinToOthers(weatherData?.main?.temp).celsius}°</h1>
+            ) : (
+              <h1>{kelvinToOthers(weatherData?.main?.temp).fahrenheit}°</h1>
+            )}
           </div>
-          <div className={style.titulo}> <h1>Weather Report</h1></div>
-
-          <div className={style.Principal}>
-            <h1>{kelvinToOthers(weatherData?.main?.temp)[temperatureUnit]}C°</h1>
-            <Principal data={weatherData} />
-          </div>
-          
           <div className={style.sidebar}>
             <Sidebar data={weatherData} />
           </div>
