b48bb005f18376c878d573262e0518a004b38702
jose luis yepez <93457870+joselyepez@users.noreply.github.com>
2022-10-17 11:09:18 -0500

    Update components/Infoprincipal.jsx
    Co-authored-by: Emmanuel Azócar <61360270+e-azocar@users.noreply.github.com>

--- components/Infoprincipal.jsx
+++ components/Infoprincipal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Infoprincipal = ({ name, value }) => (
+const MainInfo = ({ name, value }) => (
   <div className="my-6">
     <p className="font-light text-3xl leading-6">{name}</p>
     <p className="font-medium text-3xl">{value}</p>
