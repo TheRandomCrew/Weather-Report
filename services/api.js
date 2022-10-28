4368bfd393a7a68777a93b9347f85a302abe2e56
Emmanuel Azocar <azocarmel@gmail.com>
2022-10-28 19:15:43 -0400

    fix: fixed eslint errors

--- services/api.js
+++ services/api.js
@@ -1,17 +1,13 @@
-import axios from 'axios'
-import { OPENWEATHERMAP_API_KEY, OPENWEATHERMAP_BASE_URL } from '../constants'
+import axios from 'axios';
+import { OPENWEATHERMAP_API_KEY, OPENWEATHERMAP_BASE_URL } from '../constants';
 
-/** 
+/**
  * @param {Number} cityId OpenWeather City Id
 */
-export const getWeatherByCityId = async (cityId) => {
-  return await axios.get(`${OPENWEATHERMAP_BASE_URL}?id=${cityId}&appid=${OPENWEATHERMAP_API_KEY}`)
-}
+export const getWeatherByCityId = async (cityId) => axios.get(`${OPENWEATHERMAP_BASE_URL}?id=${cityId}&appid=${OPENWEATHERMAP_API_KEY}`);
 
 /**
  * @param {Number} lat City Latitude
  * @param {Number} lon City Longitude
 */
-export const getWeatherByCoords = async (lat, lon) => {
-  return await axios.get(`${OPENWEATHERMAP_BASE_URL}?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_API_KEY}`)
-}
+export const getWeatherByCoords = async (lat, lon) => axios.get(`${OPENWEATHERMAP_BASE_URL}?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_API_KEY}`);
